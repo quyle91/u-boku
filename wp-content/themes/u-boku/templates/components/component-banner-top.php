d02a1ef0c7695741e5a0f43f8a2a9f5999a5bb53
Quy Le <quylv.dsth@gmail.com>
2022-07-06 22:07:53 +0700

    update

--- wp-content/themes/u-boku/templates/components/component-banner-top.php
+++ wp-content/themes/u-boku/templates/components/component-banner-top.php
@@ -9,59 +9,50 @@
                             $item = ubk_get_sub_field('items');                            
                             ?>
                             <div class="items">
-                                <div class="bannerInfo">
-                                    <div class="bannerHead">
-                                        <div class="sub-title">
-                                            <img class="d-none d-md-block" src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/bannerMain-sub-title.svg" alt="">
-                                            <img class="d-block d-md-none" src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/bannerMain-sub-title-sp.svg" alt="">
-                                        </div>
-                                        <h3 class="bannerHead-title">PICKUP <?php echo !($key == 1)? $key : ""; ?></h3>
-                                    </div>
-                                    <p class="banner-desc">                                        
-                                        <a href="<?php echo get_permalink($item->ID); ?>">
-                                            <?php echo $item->post_title; ?>
-                                        </a>
-                                    </p>
-                                    <div class="bannerFooter">
-                                        <div class="bannerFooter-time">
-                                            <div class="date-time">
-                                                <?php 
-                                                echo get_the_date(false, $item);
-                                                ?>
+                                <div class="itemsWrap">
+                                    <div class="bannerInfo">
+                                        <div class="bannerHead">
+                                            <div class="sub-title">
+                                                <img class="d-none d-md-block" src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/bannerMain-sub-title.svg" alt="">
+                                                <img class="d-block d-md-none" src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/bannerMain-sub-title-sp.svg" alt="">
                                             </div>
-                                            <p class="author-post">
-                                                <?php 
-                                                echo get_the_author_meta('display_name',$item->post_author);
-                                                ?>
-                                            </p>
+                                            <h3 class="bannerHead-title">PICKUP <?php echo !($key == 1)? $key : ""; ?></h3>
                                         </div>
-                                        <div class="bannerFooter-control">
-                                            <button class="btn-control btn-banner-prev"><img src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/btn-prev.svg" alt=""></button>
-                                            <button class="btn-control btn-banner-next"><img src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/btn-next.svg" alt=""></button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="bannerThumb">
-                                    <div class="bannerThumb-desc">
-                                        <div class="d-none d-md-block">
-                                            <ul>
-                                                <li><span>ワーケーションメディア</span></li>
-                                                <li><span>新しい場所に出会える</span></li>
-                                                <li><span>仕事と休暇を好きな場所で。</span></li>
-                                            </ul>
-                                        </div>
-                                        <div class="d-block d-md-none">
-                                            <ul>
-                                                <li><span>仕事と休暇を好きな場所で。</span></li>
-                                                <li><span>新しい場所に出会えるワーケーションメディア</span></li>
-                                            </ul>
+                                        <p class="banner-desc">                                        
+                                            <a href="<?php echo get_permalink($item->ID); ?>">
+                                                <?php echo $item->post_title; ?>
+                                            </a>
+                                        </p>
+                                        <div class="bannerFooter">
+                                            <div class="bannerFooter-time">
+                                                <div class="date-time">
+                                                    <?php 
+                                                    echo get_the_date(false, $item);
+                                                    ?>
+                                                </div>
+                                                <p class="author-post">
+                                                    <?php 
+                                                    echo get_the_author_meta('display_name',$item->post_author);
+                                                    ?>
+                                                </p>
+                                            </div>
+                                            <div class="bannerFooter-control">
+                                                <button class="btn-control btn-banner-prev"><img src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/btn-prev.svg" alt=""></button>
+                                                <button class="btn-control btn-banner-next"><img src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/btn-next.svg" alt=""></button>
+                                            </div>
                                         </div>
                                     </div>
-                                    <span>
-                                        <a href="<?php echo get_permalink($item->ID); ?>">
-                                            <?php echo get_the_post_thumbnail( $item->ID, 'ubk_thumbnail_slide' ); ?>
-                                        </a>
-                                    </span>
+                                    <div class="bannerThumb">
+                                        <span class="bannerThumb-desc">
+                                            <img class="d-none d-md-block" src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/bannerMain-text.svg" alt="">
+                                            <img class="d-block d-md-none" src="<?php echo get_template_directory_uri()."/assets/"; ?>images/data/bannerMain-text-sp.svg" alt="">
+                                        </span>
+                                        <span>
+                                            <a href="<?php echo get_permalink($item->ID); ?>">
+                                                <?php echo get_the_post_thumbnail( $item->ID, 'ubk_thumbnail_slide' ); ?>
+                                            </a> 
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                             <?php
